89c7b8bcdfaf23c48e7b889dc886e955819eaa38
github username <github email address>
2019-10-01 20:17:11 -0400

    fixed image rendering

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends React.Component {
         item.image = image
         return item
       })
+      // set initial message if items are sold out
       this.setState({
         items: formattedItems,
         message: this.setMessage(items)
@@ -41,12 +42,16 @@ class App extends React.Component {
   }
 
   handleItemClick = (itemId) => {
+    // user can only choose another when they 
+    // haven't finalized their selection
     if(!this.state.finalized){
+      // they can unselect the item 
       if(this.state.selectedItem && this.state.selectedItem.id === itemId){
         return this.setState({
           selectedItem: null
         })
       }
+      // otherwise item is selected and re-render occurs to show it in their box
       let targetItem = this.state.items.filter(item=>item.id===itemId)[0]
       this.setState({
         selectedItem: {...targetItem}
@@ -55,14 +60,17 @@ class App extends React.Component {
   }
 
   handleFinalizeClick = () => {
+    // can only finalize if they've made a selection
     if(this.state.selectedItem){
       let currentState = this.state.finalized
       this.setState({
         finalized: !currentState
       }, ()=>{
+        // if they are finalizing at this time, that's when post is fired off to let backend know
         if(this.state.finalized){
           Adapter.postChoice(this.state.selectedItem.id)
           .catch(err=>{
+            // if there's any error from the fetch, will update the message to let the user know something's wrong
             this.setState({
               error: true
             })
